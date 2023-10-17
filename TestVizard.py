463aab653457b9f1b6083032a3d2d5d72e118f83
Matthew Cutrona <matthewbcutrona@gmail.com>
2023-10-17 01:00:58 -0400

    Added code more code for logic gate problem

--- TestVizard.py
+++ TestVizard.py
@@ -33,7 +33,7 @@ for x in [-3,-1,1,3]:
 def spinPlant(plant):
 	plant.addAction(spin)
 #causes the plants to spin
-vizact.ontimer2(0.5,19,spinPlant,vizact.choice(plants))
+#vizact.ontimer2(0.5,19,spinPlant,vizact.choice(plants))
 
 male = viz.addAvatar('vcc_male.cfg')
 male.setPosition([4.5,0,7])
@@ -88,24 +88,141 @@ mushroom = viz.addAvatar('Martial_arts_character.osgb')
 mushroom.setScale([1,1,1])
 vizact.onkeydown('3', moveMushroom)
 
+#
+def changeTexture():
+	global gateValue
+	object = viz.pick()
+	if object.valid():
+		gateValue = (gateValue + 1) % 3
+		gate.texture(gateTextures[gateValue])
+	wireColor(wire3, GateOutput(gateValue, wire1Value, wire2Value))
+	
+def AndGate(b1, b2):
+    return b1 and b2
+
+def OrGate(b1, b2):
+    return b1 or b2
+
+def XorGate(b1, b2):
+    if b1 == b2:
+        return False
+    else:
+        return True
+    
+def NotGate(b):
+    return not b
+
+def BufferGate(b):
+    return b
+
+def GateOutput(c, i1, i2):
+	if c == 0:
+		return AndGate(i1, i2)
+	elif c == 1:
+		return OrGate(i1, i2)
+	else:
+		return XorGate(i1, i2)
+
+def objColor(obj, val):
+	if val:
+		obj.color(viz.YELLOW)
+	else:
+		obj.color(viz.WHITE)
+
 andGateTex = viz.addTexture("AndGate.png")
 orGateTex = viz.addTexture("OrGate.png")
 xorGateTex = viz.addTexture("XorGate.png")
 notGateTex = viz.addTexture("NotGate.png")
-
-counter = 0
 gateTextures = [andGateTex, orGateTex, xorGateTex]
-textureInt = 0
 
-gate = viz.addTexQuad()
-gate.setPosition([10,5,10])
-gate.texture(andGateTex)
+light1 = [vizshape.addSphere(), False]
+light2 = [vizshape.addSphere(), True]
+light3 = [vizshape.addSphere(), True]
+light4 = [vizshape.addSphere(), True]
+light5 = [vizshape.addSphere(), False]
+outLight = vizshape.addSphere()
 
-def changeTexture():
-	global counter
-	object = viz.pick()
-	if object.valid():
-		counter += 1
-		gate.texture(gateTextures[counter % 3])
-		
-vizact.onkeydown('r', changeTexture)
+light1[0].setPosition(10,10,0)
+light2[0].setPosition(10,8,0)
+light3[0].setPosition(10,6,0)
+light4[0].setPosition(10,4,0)
+light5[0].setPosition(10,2,0)
+
+light1[0].setScale(.5,.5,.5)
+light2[0].setScale(.5,.5,.5)
+light3[0].setScale(.5,.5,.5)
+light4[0].setScale(.5,.5,.5)
+light5[0].setScale(.5,.5,.5)
+
+objColor(light1[0], light1[1])
+objColor(light2[0], light2[1])
+objColor(light3[0], light3[1])
+objColor(light4[0], light4[1])
+objColor(light5[0], light5[1])
+
+wire1 = [vizshape.addCylinder(), light1[1]] #[object, boolVal]
+wire1[0].setScale(.1,2,.1)
+wire1[0].setPosition(10,10,-1.5)
+wire1[0].setEuler([0,90,0])
+objColor(wire1[0], wire1[1])
+
+wire2 = [vizshape.addCylinder(), light2[1]]
+wire2[0].setScale(.1,2,.1)
+wire2[0].setPosition(10,8,-1.5)
+wire2[0].setEuler([0,90,0])
+objColor(wire2[0], wire2[1])
+
+wire3 = [vizshape.addCylinder(), light3[1]]
+wire3[0].setScale(.1,2,.1)
+wire3[0].setPosition(10,6,-1.5)
+wire3[0].setEuler([0,90,0])
+objColor(wire3[0], wire3[1])
+
+wire4 = [vizshape.addCylinder(), light4[1]]
+wire4[0].setScale(.1,2,.1)
+wire4[0].setPosition(10,4,-1.5)
+wire4[0].setEuler([0,90,0])
+objColor(wire4[0], wire4[1])
+
+wire5 = [vizshape.addCylinder(), light5[1]]
+wire5[0].setScale(.1,2,.1)
+wire5[0].setPosition(10,2,-1.5)
+wire5[0].setEuler([0,90,0])
+objColor(wire5[0], wire5[1])
+
+gate1 = [viz.addTexQuad(), 0, wire1[1], wire2[1]] #[object, what gate its on (and, or, xor), input wire val, input wire val]
+gate1[0].setPosition([10,9,-3])
+gate1[0].texture(andGateTex)
+gate1[0].setEuler([90,0,0])
+
+wire6 = [vizshape.addCylinder(), GateOutput(gate1[1], gate1[2], gate1[3])]
+wire6[0].setScale(.1,2,.1)
+wire6[0].setPosition(10,9,-5)
+wire6[0].setEuler([0,90,0])
+objColor(wire6[0], wire6[1])
+
+
+
+'''
+Test Logic gate code
+wire1 = vizshape.addCylinder()
+wire1.setPosition(5,2,2)
+wire1.setScale(.1,4,.1)
+wire1.setEuler([0,90,90])
+wire1Value = False
+
+wire2 = vizshape.addCylinder()
+wire2.setPosition(5,2.5,2)
+wire2.setScale(.1,4,.1)
+wire2.color(viz.YELLOW)
+wire2.setEuler([0,90,90])
+wire2Value = True
+
+wire3 = vizshape.addCylinder()
+wire3.setPosition(10,2.25,2)
+wire3.setScale(.1,4,.1)
+wire3.setEuler([0,90,90])
+wire3Value = False
+
+vizact.onkeydown('r', changeTexture) #ROTATE GATE OPTIONS
+'''
