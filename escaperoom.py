3ddee4064731a18c64202cdb308dd1a317e54f6c
Raf <rafaelv17@icloud.com>
2023-11-08 11:19:58 -0500

    Update escaperoom.py

--- escaperoom.py
+++ escaperoom.py
@@ -279,6 +279,8 @@ vizact.onmousedown(viz.MOUSEBUTTON_LEFT, printWeight)
 knapsackTable = viz.addChild('CustomModels/table1.osgb')
 knapsackTable.setScale([0.01, 0.0125, 0.01])
 knapsackTable.setPosition([-2, 0, 4.4])
+knapsackTable.collideMesh()
+
 '''''''''''''''END OF RIGHT WALL -- KNAPSACK PROBLEM'''''''''
 
 # Create Wall 1 with door
